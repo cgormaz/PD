0775d89e2c7ecdc4ae9936997ae6acce159569d1
burasto <cvgormaz@gmail.com>
2021-12-01 01:23:00 -0300

    Small test

--- js/slides/1_Slides_OneOff.js
+++ js/slides/1_Slides_OneOff.js
@@ -30,38 +30,74 @@ SLIDES.push({
 	onstart: function(self){
 
 		var o = self.objects;
+		Tournament.resetGlobalVariables();
+		
+		// Iterated Simulation
+		self.add({id:"iterated", type:"Iterated", x:130, y:133});
 
 		// Text
 		self.add({
-			id:"text", type:"TextBox",
+			id:"topWords", type:"TextBox",
 			x:160, y:-250, width:640, height:500, align:"center",
 			text_id:"outro_2"
 		});
-		_hide(o.text); _fadeIn(o.text, 100);
+		_hide(o.topWords); _fadeIn(o.topWords, 100);
 
-		// Photo
-		/*self.add({
-			id:"img", type:"ImageBox",
-			src: "assets/conclusion/truce.jpg",
-			x:228, y:90, width:500,
-		});
-		_hide(o.img); _fadeIn(o.img, 200);*/
 
 		// Button
 		self.add({
-			id:"button", type:"Button", x:270, y:390, 
+			id:"btnCheat", type:"Button", x:270, y:390, 
 			text_id:"outro_2_btn", size:"long",
-			message:"slideshow/scratch"
+			onclick:function(){
+				_.answer = "CHEAT";
+				publish("slideshow/next");
+			}
 		});
 		_hide(o.button); _fadeIn(o.button, 300);
 		
 		// Button2
 		self.add({
-			id:"button2", type:"Button", x:270, y:480, 
+			id:"btnCooperate", type:"Button", x:270, y:480, 
 			text_id:"outro_2_btn2", size:"long",
-			message:"slideshow/scratch"
+			onclick:function(){
+				_.answer = "COOPERATE";
+				publish("slideshow/next");
+			}
 		});
 		_hide(o.button2); _fadeIn(o.button2, 500);
+
+	},
+	onend: function(self){
+		self.remove("button");
+	}
+});
+
+SLIDES.push({
+	onstart: function(self){
+
+		var o = self.objects;
+		
+		
+		// Payoff
+		o.iterated.oneoffHighlight1(_.answer);
+
+		// Text
+		var t = o.topWords;
+		if(_.answer=="COOPERATE"){
+			t.setText(Words.get("oneoff_1_cooperated")+" "+Words.get("outro_3"));
+		}else{
+			t.setText(Words.get("oneoff_1_cheated")+" "+Words.get("outro_3"));
+		}
+
+
+		// Button
+		self.add({
+			id:"button", type:"Button", x:270, y:390, 
+			text_id:"outro_3_btn", size:"long",
+			message:"slideshow/scratch"
+		});
+		_hide(o.button); _fadeIn(o.button, 300);
+		
 
 	},
 	onend: function(self){
